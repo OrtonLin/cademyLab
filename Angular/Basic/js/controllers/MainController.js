556986a5e292fb0c5f58a937a7e1ec120ee449b0
jackeir <jackeir@gmail.com>
2015-12-23 14:21:52 +0800

    AngularJS | Basic step 6 (use ng-src and add two more objects into collection)

--- Angular/Basic/js/controllers/MainController.js
+++ Angular/Basic/js/controllers/MainController.js
@@ -13,6 +13,18 @@ app.controller('MainController', ['$scope', function($scope) {
       price: 8,
       pubdate: new Date('2013', '08', '01'),
       cover: 'img/program-or-be-programmed.jpg'
+    } ,
+    {
+      name: 'AngularJS',
+      price: 12,
+      pubdate: new Date('2015', '03', '01'),
+      cover: 'img/program-or-be-programmed.jpg'
+    } ,
+    {
+      name: 'Responsive Web Design',
+      price: 14,
+      pubdate: new Date('2014', '08', '01'),
+      cover: 'img/program-or-be-programmed.jpg'
     }
   ];
 }]);
