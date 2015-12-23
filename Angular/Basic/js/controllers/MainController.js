cab0028710af9913b1615820ccdea172b5eda2dc
jackeir <jackeir@gmail.com>
2015-12-23 14:37:48 +0800

    AngularJS | Basic step 8 (add dislike attribute and minusOne function)

--- Angular/Basic/js/controllers/MainController.js
+++ Angular/Basic/js/controllers/MainController.js
@@ -4,34 +4,42 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.plusOne = function(index) {
       $scope.products[index].likes += 1;
    };
+  $scope.minusOne = function(index) {
+      $scope.products[index].dislikes += 1;
+   };
+
   $scope.products = [
     {
       name: 'The Book of Trees',
       price: 19,
       pubdate: new Date('2014', '03', '08'),
       cover: 'img/the-book-of-trees.jpg',
-      likes :0
+      likes :0,
+      dislikes :0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013', '08', '01'),
       cover: 'img/program-or-be-programmed.jpg',
-      likes :0
+      likes :0,
+      dislikes :0
     } ,
     {
       name: 'AngularJS',
       price: 12,
       pubdate: new Date('2015', '03', '01'),
       cover: 'img/program-or-be-programmed.jpg',
-      likes :0
+      likes :0,
+      dislikes :0
     } ,
     {
       name: 'Responsive Web Design',
       price: 14,
       pubdate: new Date('2014', '08', '01'),
       cover: 'img/program-or-be-programmed.jpg',
-      likes :0
+      likes :0,
+      dislikes :0
     }
   ];
 }]);
