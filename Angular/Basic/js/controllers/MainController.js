65f61dee59c3cefd6049c93b7cdad9b2c14dfbaa
jackeir <jackeir@gmail.com>
2015-12-23 14:01:02 +0800

    AngularJS | Basic step 3 (add new object in attribute and showoff)

--- Angular/Basic/js/controllers/MainController.js
+++ Angular/Basic/js/controllers/MainController.js
@@ -1,4 +1,8 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'This text contained in title attribute!';
   $scope.promo = "A new attribute here";
+  $scope.product = {
+    name: 'The Book of Trees',
+    price: 19
+  };
 }]);
